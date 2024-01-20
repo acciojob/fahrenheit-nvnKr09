37907486e7d969cdbbe6441b87273381d0c0b7c6
Naveen Kumar <134830093+nvnKr09@users.noreply.github.com>
2024-01-20 22:34:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function toFahrenheit(celsius) {
  // Write your code here
+	 return ((celsius * 9.0 / 5.0) + 32.0);
 }
 
 // Do not change the code below
 const celsius = prompt("Enter Celsius:");
-alert(toFahrenheit(Number(celsius)));
+alert(toFahrenheit(Number(celsius))); 
